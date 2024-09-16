ee835e89bc546b8324550135aa6119f0a10c1aae
Benjamin Miller <benmiller179@gmail.com>
2024-09-16 16:30:22 -0500

    Use maps on raw vals in protobufs

--- src/main/kotlin/com/example/life/etl/CalendarETLService.kt
+++ src/main/kotlin/com/example/life/etl/CalendarETLService.kt
@@ -15,18 +15,18 @@ class CalendarETLService(
     CalendarDataSample::class.java
 ) {
     override suspend fun extract(): CalendarDataSample {
-        return utilityService.getCalendarMetrics().let {
+        return utilityService.getCalendarMetrics().let { metrics ->
             CalendarDataSample(
-                dev_total_hours = it.devTotalHours,
-                chores_total_hours = it.choresTotalHours,
-                meditation_total_hours = it.meditationTotalHours,
-                running_total_hours = it.weightTrainingTotalHours,
-                weight_training_total_hours = it.weightTrainingTotalHours,
-                dev_total_sessions = it.devTotalSessions,
-                chores_total_sessions = it.choresTotalSessions,
-                meditation_total_sessions = it.meditationTotalSessions,
-                running_total_sessions = it.runningTotalSessions,
-                weight_training_total_sessions = it.weightTrainingTotalSessions
+                dev_total_hours = metrics.getTotalHoursOrThrow("dev_total_hours"),
+                chores_total_hours = metrics.getTotalHoursOrThrow("chores_total_hours"),
+                meditation_total_hours = metrics.getTotalHoursOrThrow("meditation_total_hours"),
+                running_total_hours = metrics.getTotalHoursOrThrow("running_total_hours"),
+                weight_training_total_hours = metrics.getTotalHoursOrThrow("weight_training_total_hours"),
+                dev_total_sessions = metrics.getTotalSessionsOrThrow("dev_total_sessions"),
+                chores_total_sessions = metrics.getTotalSessionsOrThrow("chores_total_sessions"),
+                meditation_total_sessions = metrics.getTotalSessionsOrThrow("meditation_total_sessions"),
+                running_total_sessions = metrics.getTotalSessionsOrThrow("running_total_sessions"),
+                weight_training_total_sessions = metrics.getTotalSessionsOrThrow("weight_training_total_sessions")
             )
         }
     }
