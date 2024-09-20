b6c0aa849e5f1834a20a892c9f9e0b1a08e74fb0
Benjamin Miller <benmiller179@gmail.com>
2024-09-20 15:58:31 -0500

    Disable flyway for now

--- src/main/kotlin/com/example/life/config/FlywayConfig.kt
+++ src/main/kotlin/com/example/life/config/FlywayConfig.kt
@@ -1,6 +1,7 @@
 package com.example.life.config
 
 import org.flywaydb.core.Flyway
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayProperties
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -15,6 +16,7 @@ class FlywayConfig {
     fun flywayProperties() = FlywayProperties()
 
     @Bean(initMethod = "migrate")
+    @ConditionalOnProperty(name = ["spring.flyway.enabled"], havingValue = "true", matchIfMissing = true)
     fun flyway(flywayProperties: FlywayProperties, r2dbcProperties: R2dbcProperties): Flyway {
         return Flyway.configure()
             .dataSource(flywayProperties.url, r2dbcProperties.username, r2dbcProperties.password)
